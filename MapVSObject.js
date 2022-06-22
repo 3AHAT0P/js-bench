9ee87f253b22024e4b52ea9b349f7c1d32d1bb4e
3@H@T0P <i.a.kostyakov@gmail.com>
2022-06-22 19:40:45 +0300

    Update MapVSObject.js

--- MapVSObject.js
+++ MapVSObject.js
@@ -7,33 +7,47 @@ const createAndFillMap = (numberOfElements) => {
 }
 
 const createAndFillObject = (numberOfElements) => {
-  const o = new Map();
+  const o = {};
+  for (let i = 0; i < numberOfElements; i += 1) o['i-' + i] = i;
+  return o;
+}
+
+const createAndFillNullObject = (numberOfElements) => {
+  const o = Object.create(null);
   for (let i = 0; i < numberOfElements; i += 1) o['i-' + i] = i;
   return o;
 }
 
 const readSomeTimesMap = (o, numberOfElements) => {
+  let sum = 0;
   for (let i = 0; i < numberOfElements; i += 1) {
-    o.get('i-' + i);
+    sum += o.get('i-' + i);
   }
+  return sum;
 }
 
 const readSomeTimesObject = (o, numberOfElements) => {
+  let sum = 0;
   for (let i = 0; i < numberOfElements; i += 1) {
-    o['i-' + i];
+    sum += o['i-' + i];
   }
+  return sum;
 }
 
 const checkAndReadSomeTimesMap = (o, numberOfElements) => {
+  let sum = 0;
   for (let i = 0; i < numberOfElements; i += 1) {
-    if (o.has('i-' + i)) o.get('i-' + i);
+    if (o.has('i-' + i)) sum += o.get('i-' + i);
   }
+  return sum;
 }
 
 const checkAndReadSomeTimesObject = (o, numberOfElements) => {
+  let sum = 0;
   for (let i = 0; i < numberOfElements; i += 1) {
-    if (('i-' + i) in o) o['i-' + i];
+    if (('i-' + i) in o) sum += o['i-' + i];
   }
+  return sum;
 }
 
 const doBench = (fn, times = 1e6, ...args) => {
@@ -65,22 +79,30 @@ const main = () => {
   const times = 100;
   log('createAndFillMap 100 records', doBench(createAndFillMap, times, 100));
   log('createAndFillObject 100 records', doBench(createAndFillObject, times, 100));
+  log('createAndFillNullObject 100 records', doBench(createAndFillNullObject, times, 100));
   log('createAndFillMap 1e6 records', doBench(createAndFillMap, times, 1e5));
   log('createAndFillObject 1e6 records', doBench(createAndFillObject, times, 1e5));
+  log('createAndFillNullObject 1e6 records', doBench(createAndFillNullObject, times, 1e5));
   console.log('------------------------------');
   const o100 = createAndFillObject(100);
   const o1e5 = createAndFillObject(1e5);
+  const on100 = createAndFillNullObject(100);
+  const on1e5 = createAndFillNullObject(1e5);
   const m100 = createAndFillMap(100);
   const m1e5 = createAndFillMap(1e5);
   log('readSomeTimesMap 100 times from Map size 100', doBench(readSomeTimesMap, times, m100, 100));
   log('readSomeTimesObject 100 times from Object size 100', doBench(readSomeTimesObject, times, o100, 100));
+  log('readSomeTimesObject 100 times from NULL Object size 100', doBench(readSomeTimesObject, times, on100, 100));
   log('readSomeTimesMap 1e6 times from Map size 1e6', doBench(readSomeTimesMap, times, m1e5, 1e5));
   log('readSomeTimesObject 1e6 times from Object size 1e6', doBench(readSomeTimesObject, times, o1e5, 1e5));
+  log('readSomeTimesObject 1e6 times from NULL Object size 1e6', doBench(readSomeTimesObject, times, on1e5, 1e5));
   console.log('------------------------------');
   log('checkAndReadSomeTimesMap 100 times from Map size 100', doBench(checkAndReadSomeTimesMap, times, m100, 100));
   log('checkAndReadSomeTimesObject 100 times from Object size 100', doBench(checkAndReadSomeTimesObject, times, o100, 100));
+  log('checkAndReadSomeTimesObject 100 times from NULL Object size 100', doBench(checkAndReadSomeTimesObject, times, on100, 100));
   log('checkAndReadSomeTimesMap 1e6 times from Map size 1e6', doBench(checkAndReadSomeTimesMap, times, m1e5, 1e5));
   log('checkAndReadSomeTimesObject 1e6 times from Object size 1e6', doBench(checkAndReadSomeTimesObject, times, o1e5, 1e5));
+  log('checkAndReadSomeTimesObject 1e6 times from NULL Object size 1e6', doBench(checkAndReadSomeTimesObject, times, on1e5, 1e5));
 }
 
 main();
